2dad1dc320edf7d99b730cb8995d4d3d3bf575d7
yunopeak1-lgtm <yunopeak.1@gmail.com>
2026-08-24 04:34:29 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,8 +24,8 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
-        type: 4,               // Custom
+        state: "Watching PEAK.",     // this is what people actually see
+        type: 3,               // Custom
       },
     ],
   },
@@ -93,7 +93,7 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
+      primary: "#FFFFFF",
       secondary: "#2F3136",
 
       // Standard status colors for success/error/warning/info messages.
